e59145a9b0caec9cc352f4763a7357f89b8fa513
felipeazsantos <felipeasantos10@hotmail.com>
2025-08-07 14:58:13 -0300

    implementing right side header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Container from './Container';
 import Logo from './Logo';
 import HeaderMenu from './HeaderMenu';
+import SearchBar from './SearchBar';
 
 const Header = () => {
   return (
@@ -9,7 +10,9 @@ const Header = () => {
       <Container className='flex items-center justify-between'>
         <Logo />
         <HeaderMenu />
-        <div>others</div>
+        <div>
+          <SearchBar />
+        </div>
       </Container>
     </header>
   );
