90652b7b60c7033dd0f910b648ce30756ae9ad28
felipeazsantos <felipeasantos10@hotmail.com>
2025-08-07 15:45:40 -0300

    implementing sideMenu component

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -1,12 +1,24 @@
+'use client';
+
 import { AlignLeft } from 'lucide-react';
-import React from 'react';
+import React, { useState } from 'react';
+import SideMenu from './SideMenu';
+import { cn } from '@/lib/utils';
 
 const MobileMenu = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   return (
     <>
-      <button>
+      <button onClick={() => setIsSidebarOpen(prevState => !prevState)}>
         <AlignLeft className='hover:text-darkColor hoverEffect md:hidden hover:cursor-pointer' />
       </button>
+      <div className={cn('md:hidden')}>
+        <SideMenu
+          isOpen={isSidebarOpen}
+          onClose={() => setIsSidebarOpen(false)}
+        />
+      </div>
     </>
   );
 };
