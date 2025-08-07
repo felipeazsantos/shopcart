4ee34c0b2a6785cd4a729dc0b4de8ac677a93476
felipeazsantos <felipeasantos10@hotmail.com>
2025-08-07 17:57:52 -0300

    creating useOutsideClick hooj

--- components/SideMenu.tsx
+++ components/SideMenu.tsx
@@ -5,6 +5,7 @@ import { headerData } from '@/constants/data';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import SocialMedia from './SocialMedia';
+import { useOutsideClick } from '@/hook';
 
 interface SideMenuProps {
   isOpen: boolean;
@@ -13,12 +14,17 @@ interface SideMenuProps {
 
 const SideMenu: FC<SideMenuProps> = ({ isOpen, onClose }) => {
   const pathname = usePathname();
+  const sidebarRef = useOutsideClick<HTMLDivElement>(onClose);
+
   return (
     <div
       className={`fixed inset-y-0 h-screen left-0 z-50 w-full bg-black/50 text-white/80 shadow-xl 
         ${isOpen ? 'translate-x-0' : '-translate-x-full'} hoverEffect`}
     >
-      <div className='min-w-72 max-w-96 bg-black h-screen p-10 border-r border-r-shop_light_green flex flex-col gap-6'>
+      <div
+        ref={sidebarRef}
+        className='min-w-72 max-w-96 bg-black h-screen p-10 border-r border-r-shop_light_green flex flex-col gap-6'
+      >
         <div className='flex items-center justify-between gap-5'>
           <Logo className='text-white' spanClassName='group-hover:text-white' />
           <button
